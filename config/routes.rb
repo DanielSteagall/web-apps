e62c30dc2d7e9e21c109d63482734a9bde193461
DanielSteagall <daniel.steagall@kellogg.northwestern.edu>
2022-02-08 17:02:53 +0000

    class

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  resources :tacos
+  resources :dice
+  resources :cards
 end
+
+# after we need to create a controller by typing "rails generate controller tacos" in the terminal
